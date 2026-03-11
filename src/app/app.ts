e7f6824045a50c27dbb316aa9c4b7daa366b9b93
Jo Hoar <joanne.hoar@robertson.brightspace.com>
2026-03-11 15:19:24 -0600

    began customization

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('everyday-market-ng');
+  protected readonly title = signal('The FEF Market');
 }
