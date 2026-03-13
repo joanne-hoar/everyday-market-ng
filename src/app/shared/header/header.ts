6dad06baf930506fd967d48cb5828b13fa9c0e69
Joanne Hoar <joanne.hoar@gmail.com>
2026-03-13 12:52:47 -0600

    title is bound input property in header

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+
+  // declares in input (from App parent compoent)
+   title = input('');
+}
