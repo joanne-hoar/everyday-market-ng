32bd71e0d1c39fcee34ac0cdaece2e68d981ba60
Joanne Hoar <joanne.hoar@gmail.com>
2026-03-13 14:00:17 -0600

    products page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ProductsPage } from './market/products-page/products-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'products',
+        pathMatch: 'full'
+    },
+    {
+        path: 'products',
+        component: ProductsPage
+    }
+];
